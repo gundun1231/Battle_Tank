6c0dffc575047c83a55be5a5dde6522795668b76
ThePaperweight\Sam <gundun1231@gmail.com>
2017-04-04 20:08:45 -0400

    Add Tick()

--- Battle_Tanks/Source/Battle_Tanks/TankPlayerController.cpp
+++ Battle_Tanks/Source/Battle_Tanks/TankPlayerController.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Battle_Tanks.h"
 #include "TankPlayerController.h"
 
@@ -10,7 +8,6 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
-
 
 void ATankPlayerController::BeginPlay()
 {
@@ -25,33 +22,15 @@ void ATankPlayerController::BeginPlay()
 		UE_LOG(LogTemp, Warning, TEXT("Player controller possessing %s"), *(ControlledTank->GetName()));
 	}
 }
-
 void ATankPlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	AimAtCrosshair();
-	
+	AimAtCrossHair();
+	UE_LOG(LogTemp, Warning, TEXT("Tick!"));
+
 }
 
-void ATankPlayerController::AimAtCrosshair()
+void ATankPlayerController::AimAtCrossHair()
 {
-	if (!GetControlledTank()) { return; }
-	FVector HitLocation; //Out Parameter
-
-	if (GetSightRayHitLocation(HitLocation))//has a side effect of raytrace 
-	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), HitLocation.ToString());
-
-
-			//if it hits the landscape 
-				//tell controlled tank to aim at this point
-	}
-}
-
-//GetWorldLocation of linetrace through crosshair // true if it hits the landscape 
-bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
-{
-	HitLocation = FVector(1.0);
-	return true;
-
+	if (!GetControlledTank()){return;}
 }
--- Battle_Tanks/Source/Battle_Tanks/TankPlayerController.h
+++ Battle_Tanks/Source/Battle_Tanks/TankPlayerController.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "Tank.h"
@@ -7,21 +5,18 @@
 #include "TankPlayerController.generated.h"// must be the last include
 
 /**
- * 
- */
+*
+*/
 UCLASS()
 class BATTLE_TANKS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
 public:
 	ATank* GetControlledTank() const;
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
-	
-	//move barrel so a shot would hit where the crosshair intersects with the world 
-	void AimAtCrosshair();
-private:
-	//return an out parameter , true if hit landscape
-	bool GetSightRayHitLocation(FVector& HitLocation) const;
+	void AimAtCrossHair();
+
+
 };
